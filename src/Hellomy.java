69f3794e81198e3f26d61edd4c58272f63f84341
1155476 <1155476@HAE-P1155476001.hae.global>
2022-01-10 14:23:18 +0900

    hi world  메시지 추가

--- src/Hellomy.java
+++ src/Hellomy.java
@@ -3,5 +3,6 @@ public class Hellomy {
 	public static void main(String[] args) {
 		System.out.println("Hello Wweweorld");
 		System.out.println("Hi my name is kk");
+		System.out.println("Hi KKKKKKKKK name is kk");
 	}
 }
